ded69656bc8d590a32c23f1966a7a2d7b616a832
unknown <robnekyle@gmail.com>
2024-03-29 19:10:53 +0800

    Add pgn from input

--- src/pgnFetch.jsx
+++ src/pgnFetch.jsx
@@ -1,22 +1,31 @@
 import React, { useState } from 'react';
+import ChessLogic from './chess';
+import { parse } from '@mliebelt/pgn-parser';
 
 function ForPGN() {
-    const [pgn, setPGN] = useState("")
+  const [pgn, setPGN] = useState("")
 
-    const click = () => {
-      alert(pgn)
+  const click = () => {
+    try {
+      const parsedGame  = parse(pgn)
+      console.log(parsedGame);
+    } catch (error) {
+      console.log("error", error)
     }
+  }
 
-    const inputPGN = event => {
-      setPGN(event.target.value)
-    }
-    return(
-      <div className='pgnContainer'>
-        <label htmlFor="pgnValue">PGN</label>
-        <textarea id="pgnValue" onChange = {inputPGN} value = {pgn} />
-        <button onClick = {click}>Get Input Value</button>
-      </div>
-    )
+  const inputPGN = event => {
+    setPGN(event.target.value)
+  }
+
+  return(
+    <div className='pgnContainer'>
+      <label htmlFor="pgnValue">PGN</label>
+      <textarea id="pgnValue" onChange = {inputPGN} value = {pgn} />
+      <button onClick = {click}>Get Input Value</button>
+      <ChessLogic pgn={pgn}/>
+    </div>
+  )
 }
 
 export default ForPGN
