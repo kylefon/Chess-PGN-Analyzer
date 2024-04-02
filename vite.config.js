db9d07489547d963c9a4b1edbdd358fe8a3421c7
unknown <robnekyle@gmail.com>
2024-04-02 20:15:26 +0800

    Add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Chess-PGN-Viewer/",
   plugins: [react()],
+  base: "/Chess-PGN-Viewer/"
 })
